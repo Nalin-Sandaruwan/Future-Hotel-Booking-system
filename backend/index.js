536f09bd0d6553b8f12a11e3405ecfe98abdd42a
Nalin-Sandaruwan <creationbynalin@gmail.com>
2025-06-11 00:02:01 +0530

    server and data base created

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
+
+// parse application/json
+app.use(bodyParser.json())
+
+//dot env configaration
+dotenv.config({ path: './config.env' })
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/blog', {
+}).then(() => {
+    console.log('mongo is connectd...')
+}).catch((e) => {
+    console.log(e);
+})
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`server is running in the port ${port}`)
+})
