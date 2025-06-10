01076b3bc5ab056540db027831ace918e1d4ce5d
Nalin-Sandaruwan <creationbynalin@gmail.com>
2025-06-11 00:14:49 +0530

    project initialized and server created

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/blog', {
     console.log(e);
 })
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+const port = process.env.PORT ;
+const server = app. listen(port, () => {
     console.log(`server is running in the port ${port}`)
 })
